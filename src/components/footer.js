d4668e4107259f36a6caeee0da7a9e201c237f4f
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-19 13:10:49 -0500

    add last updated date to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,6 +10,7 @@ const Foot = () => {
     <Footer>
       <Content>
         <p>Thank you for visiting my site. Don't forget to shoot me a message at <a href="mailto:benwasilewski@gmail.com">benwasilewski at gmail dot com</a></p>
+        <p>This site was last updated on Sunday, April 19th 2020.</p>
       </Content>
     </Footer>  
   )
