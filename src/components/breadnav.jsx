6730197d867eabb4b31f0a24a9b71fced28cc24b
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-20 16:49:17 -0500

    add goodreads link to nav

--- src/components/breadnav.jsx
+++ src/components/breadnav.jsx
@@ -13,6 +13,7 @@ const BreadNav = () => {
         <BreadcrumbItem><Link to="/about"><b>About</b></Link></BreadcrumbItem>
         <BreadcrumbItem><a target="_blank" href="https://github.com/bwasilewski"><b>Code Samples</b></a></BreadcrumbItem>
         <BreadcrumbItem><a target="_blank" href="https://www.linkedin.com/in/benwasilewski"><b>LinkedIn</b></a></BreadcrumbItem>
+        <BreadcrumbItem><a target="_blank" href="https://www.goodreads.com/user/show/9449570-ben-wasilewski"><b>GoodReads</b></a></BreadcrumbItem>
       </ul>
     </Breadcrumb>
   )
