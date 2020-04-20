fabe68dde0b74a38b24150374efd050b3b1affe0
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-20 16:50:25 -0500

    adding more content

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -48,12 +48,15 @@ const AboutPage = () => {
             <p>Since childhood I've been fascinated by computers. I remember the release of <em>Windows 95</em>, and thought it was "so cool". By high school I was building websites for local bands and businesses.</p>
 
             <Title isSize="4">2005-2008</Title>
-            <p>I studied Interactive Arts and Media at <em>Columbia College</em> in Chicago, Illinois from 2005 to 2008, where I received my Bachelor of Arts. Before that I spent a year at the <em>University of Illinois</em> in Champaign, but it wasn't really my style.</p>
+            <p>I studied Interactive Arts and Media at <em>Columbia College</em> in Chicago, Illinois from 2005 to 2008, where I received my Bachelor of Arts. Before that I spent a year at the <em>University of Illinois</em> in Champaign, but it wasn't really my style. At Columbia I learned about many different subjects including introductory programming, graphic and user interface design, animation, 3D modeling and audio engineering.</p>
 
-            <p>While studying, I maintained a small number of websites for friends and family and participated in an internship with a local development and marketing company. Before graduation I lined up my first full time job at a local design agency where I worked on many different projects. It was here that I became a bit of a <em>jQuery</em> Evangelist within the company.</p>
-           
+            <p>While studying, I maintained a small number of websites for friends and family and participated in an internship with a local development and marketing company. Before graduation I lined up my first full time job at a local design agency where I worked on many different projects. It was here that I became a bit of a <em>jQuery</em> Evangelist. Back in these days, I spent at least 30% of my time working with <em>Flash</em> and <em>ActionScript</em>.</p>
+
             <Title isSize="4">2011</Title>
-            <p>In 2011 I left my job to work as a freelancer full time. I began building more complex applications at this point, utilizing technologies like <em>Backbone</em> and <em>Underscore</em> to more efficiently organize and modularize my code.</p>
+            <p>I have always wanted to start my own business and In 2011 I left my job to pursue this goal. I began building more complex applications at this point, utilizing technologies like <em>Backbone</em> and <em>Underscore</em> to more efficiently organize and modularize my code. I worked for a number of different clients, large and small and learned how to effectively plug into teams and produce results quickly.</p>
+            <p>In 2013 I decided to pursue another goal - living on the West Coast. Utilizing the freedom that my new business afforded me, I packed up and moved to <em>Costa Mesa, California</em>. I really began leveling up my <em>WordPress</em> skills at this point, building a number of <em>Custom Themes</em>.</p> 
+            <p>By now I had had my first taste of what it was like to build anything more complicated than a static website (ignoring all the cool stuff we used to do with <em>Flash</em> back in the day...). I was starting to really crave sharing what I've been learning with others. I began attending the Orange County Node.js where I gave a <a href="https://github.com/bwasilewski/Grunt-Presentation">presentation</a> on generating websites with <em>Grunt</em>.</p>
+            
             
             <Title isSize="4">2015</Title>
             <p>During this time I eventually began contracting for a healthcare startup full time, and soon after I accepted an offer for a permanent position as a Senior Developer on the team. We used a stack that included <em>Python</em>, <em>Mongo</em>, <em>Django</em> and <em>Backbone</em>. It was here that I became familiar with the startup environment and grew from a single-language developer to a multilingual one.</p>
