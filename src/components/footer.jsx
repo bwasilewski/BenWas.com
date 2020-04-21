d39154107dd578ea1eba099e2c69d6c3c7286cc3
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-21 18:09:30 -0500

    tighten up the content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,8 +7,8 @@ const Foot = () => {
   const year = new Date().getFullYear()
   return (
     <Section>
-      <p>Thanks for visiting! This site was last updated on Tuesday, April 21st, 2020.</p>
-      <p>If you'd like to see how this website was built, you can check out the source code <a href="https://github.com/bwasilewski/benwas.com">here</a>.</p>
+      <p><small>Thanks for visiting! This site was last updated on Tuesday, April 21st, 2020.<br />
+      If you'd like to see how this website was built, you can check out the source code <a href="https://github.com/bwasilewski/benwas.com">here</a>.</small></p>
       <p><small>Copyright &copy; { year } Ben Wasilewski</small></p>
     </Section>
   )
