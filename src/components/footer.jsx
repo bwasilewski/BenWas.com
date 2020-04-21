8eac0b951cf157cce981b592c7bccac06bda79fa
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-21 00:20:38 -0500

    print current year to copyright

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,10 +5,11 @@ import {
 } from 'bloomer'
 
 const Foot = () => {
+  const year = new Date().getFullYear()
   return (
     <Section>
       <p>Thanks for visiting! This site was last updated on Tuesday, April 21st, 2020.</p>
-      <p><small>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Ben Wasilewski</small></p>
+      <p><small>Copyright &copy; { year } Ben Wasilewski</small></p>
     </Section>
   )
 }
