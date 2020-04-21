9ff4ada4be7dc5d0a52284b54775ad49606e63f4
Benjamin Wasilewski <benwasilewski@gmail.com>
2020-04-21 00:03:05 -0500

    add copyright to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,6 +8,7 @@ const Foot = () => {
   return (
     <Section>
       <p>Thanks for visiting! This site was last updated on Tuesday, April 21st, 2020.</p>
+      <p><small>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Ben Wasilewski</small></p>
     </Section>
   )
 }
