5fb239e040fb2ec505640495ca0ba04fc3f93ab9
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2020-07-07 01:48:46 -0500

    enable rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,10 +6,64 @@ module.exports = {
     subtitle: 'Web Application Developer',
     description: `Hello! My name is Ben and I build web applications with JavaScript. I'm currently located in Burlington, Wisconsin. I have worked onsite and remotely for companies in Chicago, New York, and Los Angeles.`,
     author: `benwasilewski@gmail.com`,
+    siteUrl: `https://benwas.com`,
     updated: moment().format('MMMM Do, YYYY') 
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: {order: DESC, fields: frontmatter___date}, filter: {frontmatter: {draft: {ne: true}}},
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        title
+                        date
+                        slug
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "BenWas.com RSS Feed",
+          }
+        ]
+      }
+    },    
     {
       resolve: `gatsby-source-filesystem`,
       options: {
